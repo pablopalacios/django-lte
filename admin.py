c6719af1e21f1e31515c50ebd72b30ee1d8fbd78
Pablo Palácios <_@pablopalacios.me>
2016-04-15 15:54:37 -0300

    add site titles and header

--- admin.py
+++ admin.py
@@ -1,8 +1,12 @@
+from django.utils.translation import ugettext_lazy as _
+
 from django.contrib.admin import AdminSite
 
 
 class LTEAdminSite(AdminSite):
-    pass
+    site_title = _('AdminLTE')
+    site_header = _('Site Administration')
+    index_title = _('Dashboard')
 
 
 site = LTEAdminSite(name='lte')
