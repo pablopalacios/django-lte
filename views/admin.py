e810059cafc609569e4e2084b86e6cdca992e89e
Pablo Palácios <_@pablopalacios.me>
2016-04-26 15:39:51 -0300

    views: simplified app_index view

--- views/admin.py
+++ views/admin.py
@@ -29,23 +29,19 @@ class AppIndexView(BaseAdminView, generic.TemplateView):
 
     def get_context_data(self, **kw):
         context = super().get_context_data(**kw)
-        context['app_label'] = self.app_label
-        context['app_list'] = self.get_models()
+        context['app_name'] = self.app_name
+        context['app_model_list'] = self.get_models()
         return context
 
     def get_models(self):
-        """
-        App index view. It is a copy of AdminSite.app_index.
-        """
-        app_dict = {}
+        """ Returns a list models in which request user has access """
+        models = []
         for model, model_admin in self.registry.items():
             if self.app_label == model._meta.app_label:
                 has_module_perms = model_admin.has_module_permission(self.request)
                 if not has_module_perms:
                     raise PermissionDenied
-
                 perms = model_admin.get_model_perms(self.request)
-
                 # Check whether user has any perm for this module.
                 # If so, add the module to the model_list.
                 if True in perms.values():
@@ -65,21 +61,7 @@ class AppIndexView(BaseAdminView, generic.TemplateView):
                             model_dict['add_url'] = reverse('admin:%s_%s_add' % info, current_app=self.name)
                         except NoReverseMatch:
                             pass
-                    if app_dict:
-                        app_dict['models'].append(model_dict),
-                    else:
-                        # First time around, now that we know there's
-                        # something to display, add in the necessary meta
-                        # information.
-                        app_dict = {
-                            'name': self.app_name,
-                            'app_label': self.app_label,
-                            'app_url': '',
-                            'has_module_perms': has_module_perms,
-                            'models': [model_dict],
-                        }
-
+                    models.append(model_dict)
         # Sort the models alphabetically within each app.
-        app_dict['models'].sort(key=lambda x: x['name'])
-
-        return [app_dict]
+        models.sort(key=lambda x: x['name'])
+        return models
