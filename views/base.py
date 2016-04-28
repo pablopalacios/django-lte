944ef757216ca14f9dff7819ba83c30d86655ae4
Pablo Palácios <_@pablopalacios.me>
2016-04-28 16:51:03 -0300

    views: refactored base admin class mixins

--- views/base.py
+++ views/base.py
@@ -1,26 +1,74 @@
 from django.apps import apps
-from django.core.urlresolvers import NoReverseMatch, reverse
+from django.core.urlresolvers import NoReverseMatch, reverse, reverse_lazy
 from django.utils import six
+from django.utils.decorators import method_decorator
 from django.utils.text import capfirst
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
 
 from braces import views as braces_views
 
 
-class AdminPermissionViewMixin(braces_views.UserPassesTestMixin):
+class BaseAdminMixin(object):
+    site_title = 'DjangoLTE'
+    site_header = 'Site Administration'
+    site_url = '/'
+    name = 'lte'
+    title = 'Dashboard'
+
+    def get_site_title(self):
+        return self.site_title
+
+    def get_site_header(self):
+        return self.site_header
+
+    def get_site_url(self):
+        return self.site_header
+
+    def get_title(self):
+        return self.title
+
+    def get_context_data(self, **kw):
+        context = super().get_context_data(**kw)
+        context['site_title'] = self.get_site_title()
+        context['site_header'] = self.get_site_header()
+        context['site_url'] = self.get_site_url()
+        context['title'] = self.get_title()
+        return context
+
+
+class CSRFProtectedMixin(object):
+
+    @method_decorator(csrf_protect)
+    def dispatch(self, *args, **kw):
+        return super().dispatch(*args, **kw)
+
+
+class NoCacheAdminViewMixin(CSRFProtectedMixin):
+
+    @method_decorator(never_cache)
+    @method_decorator(csrf_protect)
+    def dispatch(self, *args, **kw):
+        return super().dispatch(*args, **kw)
+
+
+class HasPermissionMixin(braces_views.UserPassesTestMixin):
     """ CBV that implements AdminSite.has_permission """
 
-    def has_permission(self, user):
-        return user.is_active and user.is_staff
+    login_url = reverse_lazy('admin:login')
 
     def test_func(self, user):
-        return self.has_permission(user)
+        return user.is_active and user.is_staff
 
 
-class BaseAdminView(object):
-
+class PrivateAdminViewMixin(HasPermissionMixin, BaseAdminMixin):
     registry = None
-    name = 'lte'
-    title = ''
+
+    def get_context_data(self, **kw):
+        context = super().get_context_data(**kw)
+        context['title'] = self.get_title()
+        context['menu'] = self.get_menu()
+        return context
 
     def get_menu(self):
         """ Generates the admin menu based on app list. """
@@ -49,11 +97,6 @@ class BaseAdminView(object):
         app_list.sort(key=lambda x: x['name'].lower())
         return app_list
 
-    def get_title(self):
-        return self.title
 
-    def get_context_data(self, **kw):
-        context = super().get_context_data(**kw)
-        context['menu'] = self.get_menu()
-        context['title'] = self.get_title()
-        return context
+class PublicAdminViewMixin(BaseAdminMixin):
+    pass
