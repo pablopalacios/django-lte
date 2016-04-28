6a7ad2bfb3994db73b1d009c71a5a6f30d929e8a
Pablo Palácios <_@pablopalacios.me>
2016-04-28 17:15:34 -0300

    views: refactored admin views

--- views/admin.py
+++ views/admin.py
@@ -1,20 +1,20 @@
 from django.apps import apps
+from django.core.exceptions import PermissionDenied
 from django.core.urlresolvers import NoReverseMatch, reverse
 from django.utils.text import capfirst
 from django.utils.translation import ugettext_lazy as _
 from django.views import generic
 
-from .base import BaseAdminView
+from . import base
 
 
-class IndexView(BaseAdminView, generic.TemplateView):
+class IndexView(base.PrivateAdminViewMixin, base.NoCacheAdminViewMixin, generic.TemplateView):
     template_name = 'lte/index.html'
     title = _('Dashboard')
 
 
-class AppIndexView(BaseAdminView, generic.TemplateView):
+class AppIndexView(base.PrivateAdminViewMixin, base.NoCacheAdminViewMixin,  generic.TemplateView):
     template_name = 'lte/app_index.html'
-    title = _('App name not implemented')
 
     @property
     def app_label(self):
@@ -25,7 +25,7 @@ class AppIndexView(BaseAdminView, generic.TemplateView):
         return apps.get_app_config(self.app_label).verbose_name
 
     def get_title(self):
-        return _('%(app)s administration') % {'app': self.app_name}
+        return _('{app} administration'.format(app=self.app_name))
 
     def get_context_data(self, **kw):
         context = super().get_context_data(**kw)
@@ -33,14 +33,20 @@ class AppIndexView(BaseAdminView, generic.TemplateView):
         context['app_model_list'] = self.get_models()
         return context
 
+    def check_module_permissions(self, model_admin):
+        """Check permissions per module. Returns perms if there is some
+        permission, otherwise raises PermissionDenied"""
+        perms = model_admin.has_module_permission(self.request)
+        if perms:
+            return perms
+        raise PermissionDenied
+
     def get_models(self):
         """ Returns a list models in which request user has access """
         models = []
         for model, model_admin in self.registry.items():
             if self.app_label == model._meta.app_label:
-                has_module_perms = model_admin.has_module_permission(self.request)
-                if not has_module_perms:
-                    raise PermissionDenied
+                self.check_module_permissions(model_admin)
                 perms = model_admin.get_model_perms(self.request)
                 # Check whether user has any perm for this module.
                 # If so, add the module to the model_list.
@@ -48,17 +54,21 @@ class AppIndexView(BaseAdminView, generic.TemplateView):
                     info = (self.app_label, model._meta.model_name)
                     model_dict = {
                         'name': capfirst(model._meta.verbose_name_plural),
-                        'object_name': model._meta.object_name,
-                        'perms': perms,
                     }
                     if perms.get('change'):
                         try:
-                            model_dict['admin_url'] = reverse('admin:%s_%s_changelist' % info, current_app=self.name)
+                            model_dict['admin_url'] = reverse(
+                                'admin:%s_%s_changelist' % info,
+                                current_app=self.name,
+                            )
                         except NoReverseMatch:
                             pass
                     if perms.get('add'):
                         try:
-                            model_dict['add_url'] = reverse('admin:%s_%s_add' % info, current_app=self.name)
+                            model_dict['add_url'] = reverse(
+                                'admin:%s_%s_add' % info,
+                                current_app=self.name,
+                            )
                         except NoReverseMatch:
                             pass
                     models.append(model_dict)
